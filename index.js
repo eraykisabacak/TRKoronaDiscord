d8ac8391476a02dcab55c7845656e36e5b4fd60b
Ali Eray Kısabacak <eraykisabacak@hotmail.com>
2020-09-20 22:36:06 +0300

    Change Delete Line

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ client.on('ready', () => {
         case 200:
           let data = JSON.parse(body);
           oldData = data[0]['tarih'];
-          //console.log('!!!!!!!!!!!!!!!1First Data Old Data : ' + oldData);
           break;
         case 404:
           console.log('Connection Error 404 !');
@@ -29,20 +28,16 @@ client.on('ready', () => {
   );
 });
 
-/// Eğer cron data ile first data farklı ise mesaj at
-
 function cronStart() {
   var job = new CronJob(
     '* * * * * *',
     function () {
-      //console.log('You will see this message every second');
       request.get(
         'https://koronacors.herokuapp.com/?https://covid19.saglik.gov.tr/covid19api?getir=liste',
         function (error, response, body) {
           switch (response.statusCode) {
             case 200:
               let data = JSON.parse(body);
-              // console.log('Cron Data');
               cronData = data[0]['tarih'];
               if (cronData != oldData) {
                 console.log(
@@ -52,7 +47,6 @@ function cronStart() {
                   '!!! Son Dakika Yeni Korona Virus Tablosu !!!'
                 );
                 let data = JSON.parse(body);
-                //console.log(data[0]);
                 msg.channel(`Tarih : ${data[0]['tarih']}
             Günlük Test : ${data[0]['gunluk_test']}
             Günluk Vaka : ${data[0]['gunluk_vaka']}
@@ -65,10 +59,7 @@ function cronStart() {
             Hastalarda Zatürre Oranı : ${data[0]['hastalarda_zaturre_oran']}
             Ağır Hasta Sayısı : ${data[0]['agir_hasta_sayisi']}`);
                 oldData = cronData;
-              } /*else {
-                  console.log('Eski Değer Mevcut');
-                  msg.channel.send('Eski!');
-                }*/
+              }
               break;
             case 404:
               console.log('Connection Error 404 !');
@@ -107,7 +98,6 @@ client.on('message', (msg) => {
         switch (response.statusCode) {
           case 200:
             let data = JSON.parse(body);
-            //console.log(data[0]);
             msg.reply(`Tarih : ${data[0]['tarih']}
             Günlük Test : ${data[0]['gunluk_test']}
             Günluk Vaka : ${data[0]['gunluk_vaka']}
@@ -132,7 +122,6 @@ client.on('message', (msg) => {
         switch (response.statusCode) {
           case 200:
             let data = JSON.parse(body);
-            //console.log(data[0]);
             msg.reply(`Tarih : ${data[0]['tarih']}
             Günlük Test : ${data[0]['gunluk_test']}
             Günluk Vaka : ${data[0]['gunluk_vaka']}
